e8d08349b188728fc84a27ee8f33a861dfc01fb4
Sergei Calabonga <calabonga@outlook.com>
2024-08-02 14:05:13 +0300

    Engine updated

--- src/Calabonga.Commandex.Engine/DefaultDialogResult.cs
+++ src/Calabonga.Commandex.Engine/DefaultDialogResult.cs
@@ -1,4 +1,6 @@
-﻿namespace Calabonga.Commandex.Engine;
+﻿using System.Windows;
+
+namespace Calabonga.Commandex.Engine;
 
 /// <summary>
 /// // Calabonga: Summary required (DefaultDialogResult 2024-07-31 05:51)
@@ -15,4 +17,53 @@ public partial class DefaultDialogResult<T> : DefaultDialogResult
     /// // Calabonga: Summary required (DefaultDialogResult 2024-07-31 05:52)
     /// </summary>
     public T? Result { get; set; }
+}
+
+public abstract class DefaultDialogResult : ViewModelBase, IDialogResult
+{
+    /// <summary>
+    /// // Calabonga: Summary required (IDialogResult 2024-07-31 05:53)
+    /// </summary>
+    public abstract string DialogTitle { get; }
+
+    /// <summary>
+    /// Default value <see cref="ResizeMode.NoResize"/>
+    /// </summary>
+    public virtual ResizeMode ResizeMode => ResizeMode.NoResize;
+
+    /// <summary>
+    /// Default value <see cref="SizeToContent.WidthAndHeight"/>
+    /// </summary>
+    public virtual SizeToContent SizeToContent => SizeToContent.WidthAndHeight;
+
+    /// <summary>
+    /// Default value <see cref="WindowStyle.ToolWindow"/>
+    /// </summary>
+    public virtual WindowStyle WindowStyle => WindowStyle.ToolWindow;
+}
+
+/// <summary>
+/// // Calabonga: Summary required (DefaultDialogResult 2024-08-02 01:48)
+/// </summary>
+public abstract class DefaultDialogWithValidationResult : ViewModelWithValidatorBase, IDialogResult
+{
+    /// <summary>
+    /// // Calabonga: Summary required (IDialogResult 2024-07-31 05:53)
+    /// </summary>
+    public abstract string DialogTitle { get; }
+
+    /// <summary>
+    /// Default value <see cref="ResizeMode.NoResize"/>
+    /// </summary>
+    public virtual ResizeMode ResizeMode => ResizeMode.NoResize;
+
+    /// <summary>
+    /// Default value <see cref="SizeToContent.WidthAndHeight"/>
+    /// </summary>
+    public virtual SizeToContent SizeToContent => SizeToContent.WidthAndHeight;
+
+    /// <summary>
+    /// Default value <see cref="WindowStyle.ToolWindow"/>
+    /// </summary>
+    public virtual WindowStyle WindowStyle => WindowStyle.ToolWindow;
 }
