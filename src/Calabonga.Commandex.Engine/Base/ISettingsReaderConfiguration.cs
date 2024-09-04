d8792694c36d2c8d7875049f58d714667c428ecd
Sergei Calabonga <dev@calabonga.net>
2024-09-04 13:27:08 +0700

    ISettingsReaderConfiguration abstraction created

---
+++ src/Calabonga.Commandex.Engine/Base/ISettingsReaderConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace Calabonga.Commandex.Engine.Base;
+
+/// <summary>
+/// Default interface for settings reader configuration
+/// </summary>
+public interface ISettingsReaderConfiguration
+{
+    /// <summary>
+    /// Returns file name where settings stored
+    /// </summary>
+    /// <returns></returns>
+    string GetEnvironmentFileName();
+
+    /// <summary>
+    /// Returns file extension name for file where settings stored
+    /// </summary>
+    /// <returns></returns>
+    string GetExtensionFileName();
+}
