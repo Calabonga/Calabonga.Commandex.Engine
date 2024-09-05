e36eecebd2403b68c85cc5e8051a29c469620edf
Sergei Calabonga <dev@calabonga.net>
2024-09-05 14:53:30 +0700

    DialogResult serialization issue

--- src/Calabonga.Commandex.Engine/Dialogs/DefaultDialogResult.cs
+++ src/Calabonga.Commandex.Engine/Dialogs/DefaultDialogResult.cs
@@ -1,4 +1,5 @@
 ﻿using Calabonga.Commandex.Engine.Base;
+using System.Text.Json.Serialization;
 using System.Windows;
 
 namespace Calabonga.Commandex.Engine.Dialogs;
@@ -11,21 +12,25 @@ public abstract class DefaultDialogResult : ViewModelBase, IDialogResult
     /// <summary>
     /// Default value <see cref="ResizeMode.NoResize"/>
     /// </summary>
+    [JsonIgnore]
     public virtual ResizeMode ResizeMode => ResizeMode.NoResize;
 
     /// <summary>
     /// Default value <see cref="SizeToContent.WidthAndHeight"/>
     /// </summary>
+    [JsonIgnore]
     public virtual SizeToContent SizeToContent => SizeToContent.WidthAndHeight;
 
     /// <summary>
     /// Default value <see cref="WindowStyle.ToolWindow"/>
     /// </summary>
+    [JsonIgnore] 
     public virtual WindowStyle WindowStyle => WindowStyle.ToolWindow;
 
     /// <summary>
     /// Window instance that open this dialog
     /// </summary>
+    [JsonIgnore]
     public object? Owner { get; set; }
 
     /// <summary>
--- src/Calabonga.Commandex.Engine/Dialogs/DefaultDialogWithValidationResult.cs
+++ src/Calabonga.Commandex.Engine/Dialogs/DefaultDialogWithValidationResult.cs
@@ -1,4 +1,5 @@
 ﻿using Calabonga.Commandex.Engine.Base;
+using System.Text.Json.Serialization;
 using System.Windows;
 
 namespace Calabonga.Commandex.Engine.Dialogs;
@@ -9,30 +10,34 @@ namespace Calabonga.Commandex.Engine.Dialogs;
 public abstract class DefaultDialogWithValidationResult : ViewModelWithValidatorBase, IDialogResult
 {
     /// <summary>
-    /// // Calabonga: Summary required (IDialogResult 2024-07-31 05:53)
-    /// </summary>
-    public abstract string DialogTitle { get; }
-
-    /// <summary>
     /// Default value <see cref="ResizeMode.NoResize"/>
     /// </summary>
+    [JsonIgnore]
     public virtual ResizeMode ResizeMode => ResizeMode.NoResize;
 
     /// <summary>
     /// Default value <see cref="SizeToContent.WidthAndHeight"/>
     /// </summary>
+    [JsonIgnore]
     public virtual SizeToContent SizeToContent => SizeToContent.WidthAndHeight;
 
     /// <summary>
     /// Default value <see cref="WindowStyle.ToolWindow"/>
     /// </summary>
+    [JsonIgnore]
     public virtual WindowStyle WindowStyle => WindowStyle.ToolWindow;
 
     /// <summary>
     /// Dialog instance
     /// </summary>
+    [JsonIgnore]
     public object? Owner { get; set; }
 
-    /// <summary>Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.</summary>
-    public abstract void Dispose();
+    /// <summary>
+    /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+    /// </summary>
+    public virtual void Dispose()
+    {
+
+    }
 }
