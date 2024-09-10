e3777491e380fc3d9c04a7ba32f675ad39a325a9
Sergei Calabonga <s.calabonga@qwell.ru>
2024-09-10 13:01:39 +0300

    Nuget Feed configuration

--- src/Calabonga.Commandex.Engine/NugetDependencies/NugetDependency.cs
+++ src/Calabonga.Commandex.Engine/NugetDependencies/NugetDependency.cs
@@ -1,11 +1,17 @@
 ﻿namespace Calabonga.Commandex.Engine.NugetDependencies;
 
 /// <summary>
-/// // Calabonga: Summary required (NugetDependency 2024-08-07 10:19)
+/// Dependency description for Commandex Shell
 /// </summary>
-public class NugetDependency
+public sealed class NugetDependency
 {
+    /// <summary>
+    /// Nuget-package full name, like "Calabonga.TokenGeneratorCore"
+    /// </summary>
     public required string Name { get; set; }
 
+    /// <summary>
+    /// Nuget-package version, like "1.0.0"
+    /// </summary>
     public required string Version { get; set; }
 }
